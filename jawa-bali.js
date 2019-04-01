8f396b294685834db443a32505329ced452cad64
zimi95 <35018323+zimi95@users.noreply.github.com>
2019-04-01 16:59:59 +0700

    d
    a

---
+++ jawa-bali.js
@@ -0,0 +1,76 @@
+/**
+ * @license
+ * Copyright (c) 2018 amCharts (Antanas Marcelionis, Martynas Majeris)
+ *
+ * This sofware is provided under multiple licenses. Please see below for
+ * links to appropriate usage.
+ *
+ * Free amCharts linkware license. Details and conditions:
+ * https://github.com/amcharts/amcharts4/blob/master/LICENSE
+ *
+ * One of the amCharts commercial licenses. Details and pricing:
+ * https://www.amcharts.com/online-store/
+ * https://www.amcharts.com/online-store/licenses-explained/
+ *
+ * If in doubt, contact amCharts at contact@amcharts.com
+ *
+ * PLEASE DO NOT REMOVE THIS COPYRIGHT NOTICE.
+ * @hidden
+ */
+am4internal_webpackJsonp(["9eab"], {
+    gaX1: function(e, i, a) {
+        "use strict";
+        Object.defineProperty(i, "__esModule", {
+            value: !0
+        });
+        window.am4geodata_indonesiaLow = {
+            "type": "FeatureCollection",
+            "features": [
+              {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                  "type": "Polygon",
+                  "coordinates": [
+                    [
+                      [
+                        106.171875,
+                        -5.637852598770853
+                      ],
+                      [
+                        104.61181640625,
+                        -6.620957270326323
+                      ],
+                      [
+                        105.16113281249999,
+                        -7.928674801364035
+                      ],
+                      [
+                        115.55419921875,
+                        -9.102096738726443
+                      ],
+                      [
+                        115.97167968750001,
+                        -7.754537346539373
+                      ],
+                      [
+                        113.88427734374999,
+                        -6.533645130567532
+                      ],
+                      [
+                        106.2158203125,
+                        -5.462895560209544
+                      ],
+                      [
+                        106.171875,
+                        -5.637852598770853
+                      ]
+                    ]
+                  ]
+                }
+              }
+            ]
+          }
+    }
+}, ["gaX1"]);
+//# sourceMappingURL=indonesiaLow.js.map
